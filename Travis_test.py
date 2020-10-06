45e754aee9262907f57dab5ad666d7664118397d
Aditya Kumar <akumar98@wisc.edu>
2020-10-06 17:05:30 -0500

    Adding python script to test Travis

---
+++ Travis_test.py
@@ -0,0 +1,13 @@
+# This is just a test .py script for testing Travis
+
+def print_input(s):
+    print("printing from inside function")
+    print(s)
+
+s="Hello Travis"
+print_input(s)
+print("printing from outside function")
+print(s)
+
+
+
