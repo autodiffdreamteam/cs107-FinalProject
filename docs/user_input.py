b380e91a8f474277b246894fb3ff75cb80109894
Aditya Kumar <akumar98@wisc.edu>
2020-11-17 01:07:58 -0600

    Adding code snippet for user function input

---
+++ docs/user_input.py
@@ -0,0 +1,14 @@
+#code snippet to get function input from user. Could pass in user input
+#to the main library
+print("=========================================")
+print("Math symbols for function input:")
+print("Addition: +")
+print("Subtraction: -")
+print("Multiplication: *")
+print("Division: /")
+print("Exponentiation: **")
+print("=========================================")
+
+inp_function = input("Please enter a function:   ")
+print("You have entered: ")
+print(inp_function)
