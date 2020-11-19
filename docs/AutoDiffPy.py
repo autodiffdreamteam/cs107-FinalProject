55e51137d4f8534f793b71a5558a2c0420204dd3
Carlos Robles <carlosrobles@college.harvard.edu>
2020-11-19 14:55:43 -0800

    Commenting, cleaning parse input

--- docs/AutoDiffPy.py
+++ docs/AutoDiffPy.py
@@ -1,17 +1,26 @@
 import numpy as np
 from math import log, sin, cos, exp, tan
-import compiler
-import parser
 import math
 
 
 class AutoDiffPy():
-
-    #def __init__(self, val, der=1):
-    #    self.val = val
-    #    self.der = der
     
     def __init__(self, val, der = 1, input_function = None):
+        '''
+        Constructs an AutoDiffPy object to perform forward automatic differentation.
+
+        Parameters:
+        self(AutoDiffPy): Self
+        val (int/float): This is the seed value that we will evaluate at. 
+        der (int/float): The derivative.
+        input_function (str): An input function in string format in the general form of 'log(sin(3*x^2 + 5*x - x^5 * 5*x))' where the inner function can be any polynomial, and there can be as many operations to the left of the parentheses as needed.
+        Refer to the AutoDiffPy input_function user documentation for further syntax details.
+
+        Returns:
+        AutoDiffPy object with a value and derivative.
+        '''
+
+
         if input_function !=None:
             self.val = 0
             self.der = 0
@@ -19,8 +28,6 @@ class AutoDiffPy():
         else:        
             self.val = val
             self.der = der
-        
-        
 
     # Overload add dunder method
     def __add__(self, other):
@@ -34,6 +41,14 @@ class AutoDiffPy():
     # Handle right side addition
     def __radd__(self, other):
         return self.__add__(other)
+
+    # Overload the subtraction dunder method
+    def __sub__(self, other):
+        return self.__add__(-other)
+
+    # Overload the right subtraction dunder method
+    def __rsub__(self, other):
+        return self.__sub__(other)
 
     # Overload the multiplication dunder method
     def __mul__(self, other):
@@ -50,6 +65,41 @@ class AutoDiffPy():
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    # Overload the division dunder method
+    def __truediv__(self, other):
+        try:
+            # Use the quotient rule to calculate derivative
+            self.der = (self.der*other.val - self.val*other.der) / other.val**2
+            self.val /= other.val
+        except AttributeError:
+            self.val /= other
+            self.der /= other
+        return self
+
+    # Overload the right side division dunder method
+    def __rtruediv__(self, other):
+        return self.__truediv__(other)
+
+    # Overload the power dunder method
+    def __pow__(self, other):
+        try:
+            # Use the chain rule to calculate derivative
+            self.der = np.log(self.val)*(self.val**other.val)
+            self.val **= other.val
+        except AttributeError:
+            self.val **= other
+            self.der = other.val*(self.val**(other - 1))
+        return self
+
+    # Overload the right power dunder method
+    def __rpow__(self, other):
+        return self.__pow__(other)
+    
+    # Overload the negation dunder method
+    def __neg__(self):
+        self.val *= -1
+        self.der *= -1
+
     # Define a function value getter
     def val(self):
         return self.val
@@ -58,32 +108,59 @@ class AutoDiffPy():
     def der(self):
         return self.der
 
+    # Define sin function for true value and derivative
     def sin(self, x):
         return AutoDiffPy(np.sin(x.val), np.cos(x.val)*x.der)
 
+    # Define cos function for true value and derivative
     def cos(self, x):
         return AutoDiffPy(np.cos(x.val), -np.sin(x.val)*x.der)
 
+    # Define tan function for true value and derivative
     def tan(self, x):
-        return AutoDiffPy(np.tan(x.val), np.arctan(x.val)*x.der)
+        return AutoDiffPy(np.tan(x.val), (np.arccos(x.val)**2)*x.der)
 
+    # Define log function for true value and derivative
     def log(self, x):
         return AutoDiffPy(np.log(x.val), (1/x.val)*x.der)
 
+    # Define exp function for true value and derivative
     def exp(self, x):
         return AutoDiffPy(np.exp(x.val), (np.exp(x.val))*x.der)
     
+    # Define method to access AutoDiffPy functions by string
     def derivative_dict(self, function):
+        '''
+        Takes in an a string representing a function and returns a tuple of the update value and derivative
+
+        Parameter(s):
+
+        self(AutoDiffPy): Self, an AutoDiffPy object
+        function(str): A 3 character string denoting an elementary function like cos or log
+        
+        Returns:
+
+        autodiff_new.val(float/int): The new value after applying the specified function
+        autodiff_new.der(float/int): The new derivative after applying the specified function
+        '''
+
         derivative_dict = {'sin': self.sin, 'cos': self.cos, 'tan': self.tan, 'log': self.log, 'exp': self.exp}
         autodiff_new = derivative_dict[function](self)
         return autodiff_new.val, autodiff_new.der
 
-    def function_dict(self, function):
-        derivative_dict = {'sin': np.sin, 'cos': np.cos, 'tan': np.tan, 'log': np.log, 'exp': np.exp}
-        
-        return derivative_dict[function](self)
+    def parse_input(self, input_function, a): 
+        '''
+        parse_input parses an input function of the form (write format parameters) , sequentially feeding all operations and derivatives of such to perform forward automatic differentation on the given input_function.
 
-    def parse_input(self, input_function, a): # figure out when this is CALLED!
+        Parameters:
+        self(AutoDiffPy): Self, an AutoDiffPy object
+        input_function(str):
+        a(int/float): The value which the input function will be evaluated at.
+
+        Returns: Void
+        '''
+
+
         # Example input: 'log(x^2 + 5)', seed = a
 
         lefts_par = input_function.split('(')
@@ -96,11 +173,12 @@ class AutoDiffPy():
         center = math.floor(len(parsed_eq)/2)
         self.val = self.simplify(parsed_eq[center], a)
         self.der = self.simplify(parsed_eq[center], a, der=True)
-        
+
         for func in parsed_eq[0:center]:
             self.val, self.der = self.derivative_dict(func) 
         # no return value 
 
+    # Splits a coefficient
     def coef_split(self, x):
         split = x.split('*')
         if len(split) > 1:
@@ -116,7 +194,17 @@ class AutoDiffPy():
             power = 1
         return coef, power
 
+    # Evaluates an x term
     def coef_evaluate(self, x, a, der = False):
+        '''
+        Evaluates an x term with an optional coefficent and power. If der == True evaluates the derivative instead.
+
+        Parameters:
+        self(AutoDiffPy)
+        x(str): A string representing an x term with an optional coefficient and power
+        a(float/int): The value to evaluate x at
+        der(bool): If True, the derivative value is evaluated using the power rule. 
+        '''
         if not 'x' in x:
             if der == True:
                 return 0
@@ -140,7 +228,16 @@ class AutoDiffPy():
             value = (a**power) * coef
         return value
 
-    def power_rule(self, x, realx): # x is a string like 3*x^2
+    # Performs the power rule on a string representing an x term
+    def power_rule(self, x, realx):
+        '''
+        Params:
+        x(str): A string representing an x term with a optional coefficient and power such as '3*x^5'
+        realx(int/float): The seed value for the expression to be evaluated at. 
+        
+        Returns:
+        Value after application of power rule
+        '''
         split = x.split('*')
         coef = float(split[0])
         power = split[1].split('^')
@@ -150,8 +247,23 @@ class AutoDiffPy():
             return coef * realx
         return (coef * power)*realx**(power-1)
 
+
     def simplify(self, x, seed = None, der = False): # where x is string of polynomial such as '3*x^2 * 5*x^3 + 5', if seed == True, we evaluate
-    # split by addition/subtraction
+        '''
+        Simplifies a polynomial expression
+
+        Parameters: 
+        self(AutoDiffPy): An AutoDiffPy object
+        x(str): A string representation of a polynomial expression
+        seed(int/float): The value to evaluated at
+        der (bool): If der == True, then it will both simplify the polynomial, and evaluate its derivative.
+
+        Returns:
+        runningval: The polynomial expression evaluted at the seed if seed != None
+        OR
+        simplified: The polynomial expression simplified and returned as a string  
+        '''
+        # split by addition/subtraction
         minus_split = x.split('-')
         plusminus_split = []    
         for i in range(len(minus_split)):
@@ -164,6 +276,8 @@ class AutoDiffPy():
                 plusminus_split.append('-')
 
         final_array = []
+
+        # split my multiplication/division
         for section in plusminus_split:
             bsymb = False
             multdiv_split = []
@@ -182,7 +296,7 @@ class AutoDiffPy():
                 final_array.append(section)
             else:
                 for i in range(len(multdiv_split)):
-                    # check if symbol
+                    # Set symbol to false if index is at an operator
                     symbol = False
                     if '*' == multdiv_split[i]:
                         symbol = True
@@ -270,38 +384,10 @@ class AutoDiffPy():
 
 # Demo
 a = 2.0 # value to evaluate f at
-alpha = 2.0 # slope value
-beta = 3.0 # intercept value
 
-#x = AutoDiffPy(a) 
-#x = AutoDiffPy.tan(x)
-#f2 = AutoDiffPy.sin(x)
-#f = alpha * x + beta
-#print(f2.val, f2.der)
-
-neweq = 'log(3*x^5 + 5)'
-#neweq = '3*x^5 * 2*x^4 + 5*x^2 - 3*x^2 * x'
+neweq = 'sin(x^5 + 5)'
 
 
 MyDiff = AutoDiffPy(a, input_function=neweq)
 print(MyDiff.der, "this is derivative")
 print(MyDiff.val, "this is value")
-
-#print(simplify(neweq))
-#value = power_rule('3*x^2', 2)
-#print(value)
-
-    #return AutoDiffPy(c*(a) * x**(a-1) )
-
-
-
-
-
-
-
-# f1 = 3x + 5x^2  /// val: 3*seed, + 5*seed^2       /// der: power_rule(3*x) + power_rule(5*seed^2)
-# f2 = sin(f1)    ///  val: sin(f1)                 /// der: der(sin(f1))
-# f3 = evaluate(f2)**3  /// val: f3**2              /// **2 ? Would we use power rule here?
-# f4= cos(f3)           /// val: cos(f3)            /// der: der(cos(f3))     
-# derivative = log(f4)  /// log(f4)                 /// der: der(log(f4))
-
