74611be11b486c94a3129b7a558d577e606c058b
blakebullwinkel <jblake1@live.com>
2020-12-03 13:35:06 -0500

    updating AutoDiff class name in Function.py

--- ad_library/Function.py
+++ ad_library/Function.py
@@ -1,24 +1,24 @@
 import numpy as np
-from AutoDiff import AutoDiffPy
+from AutoDiff import AutoDiff
 
 class Function:
 
     # Elemental exponential function
     def exp(x):
-        return AutoDiffPy(np.exp(x.val), x.der*np.exp(x.val))
+        return AutoDiff(np.exp(x.val), x.der*np.exp(x.val))
 
     # Elemental logarithm function
     def log(x):
-        return AutoDiffPy(np.log(x.val), x.der/x.val)
+        return AutoDiff(np.log(x.val), x.der/x.val)
 
     # Elemental sine function
     def sin(x):
-        return AutoDiffPy(np.sin(x.val), np.cos(x.val)*x.der)
+        return AutoDiff(np.sin(x.val), np.cos(x.val)*x.der)
 
     # Elemental cosine function
     def cos(x):
-        return AutoDiffPy(np.cos(x.val), -np.sin(x.val)*x.der)
+        return AutoDiff(np.cos(x.val), -np.sin(x.val)*x.der)
 
     # Elemental tangent function
     def tan(x):
-        return AutoDiffPy(np.tan(x.val), np.arctan(x.val)*x.der)
+        return AutoDiff(np.tan(x.val), np.arctan(x.val)*x.der)
