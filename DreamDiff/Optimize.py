0dccf5504f2c77fc77877e140611c3db15430027
blakebullwinkel <jblake1@live.com>
2020-12-11 16:06:41 -0500

    adding more animation functions to Optimize

--- DreamDiff/Optimize.py
+++ DreamDiff/Optimize.py
@@ -31,7 +31,7 @@ class Optimize:
         If a root is found, the algorithm returns its x-value, a list
         containing the previous points at which the derivative was 
         evaluated, a list of the function's values at those points, and
-        a list of the function's derivatives at those points.
+        a list of the function's derivatives at those points in a 4-tuple.
         If no root is found or a zero derivative is reacahed, None is 
         returned.
         '''
@@ -64,7 +64,7 @@ class Optimize:
             if abs(yn) < epsilon:
                 print('Found solution after {} iterations.'.format(i))
                 print('Solution is: {}'.format(xn))
-                return xn, xn_history, yn_history, der_history
+                return (xn, xn_history, yn_history, der_history)
             
             # If a zero derivative is reached, terminate the algorithm and
             # return None
@@ -100,50 +100,58 @@ class Optimize:
 
         RETURNS
         =======
-        A matplotlib animataion showing our Newton's method located
-        a root of the input function.
+        A matplotlib animataion showing how Newton's method located
+        a root of the input function if a root was found, otherwise
+        None.
         '''
         # Get the root and history of x-values, y-values, and derivatives 
         # for the input function using newtons_method
-        root, x_vals, y_vals, grad_vals = Optimize.newtons_method(f, x0, epsilon, max_iters)
-
-        # Create a DreamDiff object to access private methods
-        x = ad(1.0)
-
-        # Create a list of x-vals searched
-        t_vals = np.arange(min(x_vals)-2, max(x_vals)+2, 0.1)
+        results = Optimize.newtons_method(f, x0, epsilon, max_iters)
         
-        # Parse the input function string
-        f_parsed = x._parse_input(f)
+        # Check if newtons_method returned None
+        if results == None:
+            print("Newton's method did not find a root.")
+            return None
+        else:
+            root, x_vals, y_vals, grad_vals = results[0], results[1], results[2], results[3]
 
-        # Calculate the values of the input function in the range of x-vals searched
-        f_vals = [x._evaluate_function(f_parsed, t).val[0] for t in t_vals]
-      
-        # Initialize the animation
-        fig = plt.figure()
-        fig.canvas.draw()
-        fig.suptitle("Newton's Method for f(x)="+f)
+            # Create a DreamDiff object to access private methods
+            x = ad(1.0)
 
-        # Run the animation for the duration of 'runtime'
-        while runtime > 0:
+            # Create a list of x-vals searched
+            t_vals = np.arange(min(x_vals)-2, max(x_vals)+2, 0.1)
+            
+            # Parse the input function string
+            f_parsed = x._parse_input(f)
 
-            # Plot the animation, where each frame shows the function and
-            # tangent line at which the gradient was evaluated
-            for i in range(len(x_vals)):
-                grad = grad_vals[i]
-                intercept = y_vals[i] - grad*x_vals[i]
-                tangent_y = grad*t_vals + intercept
-                plt.cla()
-                plt.grid(True)
-                plt.plot(t_vals, f_vals, label='Function')
-                plt.plot(t_vals, tangent_y, '-r', label='Tangent')
-                plt.xlim(t_vals[0], t_vals[-1])
-                plt.ylim(min(f_vals), max(f_vals))
-                plt.xlabel('x')
-                plt.ylabel('f(x)')
-                plt.legend()
-                plt.pause(0.2)
-                runtime -= 1
+            # Calculate the values of the input function in the range of x-vals searched
+            f_vals = [x._evaluate_function(f_parsed, t).val[0] for t in t_vals]
+          
+            # Initialize the animation
+            fig = plt.figure()
+            fig.canvas.draw()
+            fig.suptitle("Newton's Method for f(x)="+f)
+
+            # Run the animation for the duration of 'runtime'
+            while runtime > 0:
+
+                # Plot the animation, where each frame shows the function and
+                # tangent line at which the gradient was evaluated
+                for i in range(len(x_vals)):
+                    grad = grad_vals[i]
+                    intercept = y_vals[i] - grad*x_vals[i]
+                    tangent_y = grad*t_vals + intercept
+                    plt.cla()
+                    plt.grid(True)
+                    plt.plot(t_vals, f_vals, label='Function')
+                    plt.plot(t_vals, tangent_y, '-r', label='Tangent')
+                    plt.xlim(t_vals[0], t_vals[-1])
+                    plt.ylim(min(f_vals), max(f_vals))
+                    plt.xlabel('x')
+                    plt.ylabel('f(x)')
+                    plt.legend()
+                    plt.pause(0.2)
+                    runtime -= 1
 
 
     def grad_descent(f, x0, epsilon=0.000001, max_iters=500, eta=0.1):
@@ -168,30 +176,76 @@ class Optimize:
         If a minimum is found, the algorithm returns its x-value, a list
         containing the previous points at which the derivative was 
         evaluated, a list of the function's values at those points, and
-        a list of the function's derivatives at those points.
+        a list of the function's derivatives at those points in a 4-tuple.
         If no minimum is found, None is returned.
         '''
+        # Initialize xn at the starting point
         xn = x0
+
+        # Create a DreamDiff object to access private methods
         x = ad(1.0)
-        fn_parsed = x._parse_input(fn)
+
+        # Parse the input function string
+        f_parsed = x._parse_input(f)
+
+        # Initialize empty lists to hold results
         xn_history = []
+        yn_history = []
+        der_history = []
         
+        # Run the algorithm at most 'max_iters' times
         for i in range(max_iters):
+
+            # Calculate values, derivatives, and store in lists
             xn_history.append(xn)
-            new_fxn = x._evaluate_function(fn_parsed, xn)
-            Dfxn = new_fxn.der[0] 
-            new_xn = xn - eta*Dfxn
+            new_f = x._evaluate_function(f_parsed, xn)
+            yn = new_f.val[0]
+            der = new_f.der[0]
+            yn_history.append(yn)
+            der_history.append(der)
+            
+            # Apply the gradient descent update (scaled by learning rate)
+            new_xn = xn - eta*der
+
+            # If threshold is met, terminate the algorithm and return results
             if abs(new_xn - xn) < epsilon:
                 print('Found solution after {} iterations.'.format(i))
                 print('Solution is: {}'.format(new_xn))
-                return new_xn
+                return (new_xn, xn_history, yn_history, der_history)
+
+            # Update xn
             xn = new_xn
+
+        # If no solution is found within 'max_iters', tell the user and return None
         print('No solution found after max iterations.')
+        return None
 
 
     def nesterov_grad_descent(fn, x0, epsilon, max_iters, eta):
         '''
-        WORKING
+        Implements Nesterov's accelerated gradient descent 
+        optimization algorithm.
+
+        INPUTS
+        ======
+        f: str, required
+            Input function on which to perform gradient descent.
+        x0: int or float, required
+            Starting x-value at which to initialize the algorithm.
+        epsilon: int or float, optional
+            Solution accuracy threshold.
+        max_iters: int, optional
+            The maximum number of times to run the algorithm.
+        eta: int or float, optional
+            The learning rate, which controls the algorithm step size.
+
+        RETURNS
+        =======
+        If a minimum is found, the algorithm returns its x-value, a list
+        containing the previous points at which the derivative was 
+        evaluated, a list of the function's values at those points, and
+        a list of the function's derivatives at those points in a 4-tuple.
+        If no minimum is found, None is returned.
         '''
         xn = x0
         x = ad(1.0)
@@ -218,45 +272,100 @@ class Optimize:
             yn = new_yn
             t = new_t
         print('No solution found after max iterations.')
+    
+
+    def animate_grad_desc(f, x0, epsilon=0.000001, max_iters=500, eta=0.1, runtime=20): 
+        '''
+        Creates an animation of the gradient descent method for scalar
+        functions by plotting the function and showing the tangent lines
+        at which the gradient was evaluated.
+
+        INPUTS
+        ======
+        f: str, required
+            Input function on which to perform gradient descent.
+        x0: int or float, required
+            Starting x-value at which to initialize the algorithm.
+        epsilon: int or float, optional
+            Solution accuracy threshold.
+        max_iters: int, optional
+            The maximum number of times to run the algorithm.
+        eta: int or float, optional
+        runtime: int, optional
+            The length of time to run the animation.
+
+        RETURNS
+        =======
+        A matplotlib animataion showing how gradient descent located
+        a minimum of the input function.
+        '''
+        # Get the minimum and history of x-values, y-values, and derivatives 
+        # for the input function using grad_descent
+        results = Optimize.grad_descent(f, x0, epsilon, max_iters, eta)
+
+        # Check if grad_descent returned None
+        if results == None:
+            print('Gradient descent did not find a minimum.')
+            return None
+        else:
+            minimum, x_vals, y_vals, grad_vals = results[0], results[1], results[2], results[3]
+
+            # Create a DreamDiff object to access private methods
+            x = ad(1.0)
+
+            # Create a list of x-vals searched
+            t_vals = np.arange(min(x_vals)-2, max(x_vals)+2, 0.1)
+            
+            # Parse the input function string
+            f_parsed = x._parse_input(f)
+
+            # Calculate the values of the input function in the range of x-vals searched
+            f_vals = [x._evaluate_function(f_parsed, t).val[0] for t in t_vals]
+          
+            # Initialize the animation
+            fig = plt.figure()
+            fig.canvas.draw()
+            fig.suptitle("Gradient Descent for f(x)="+f)
+
+            # Run the animation for the duration of 'runtime'
+            while runtime > 0:
+
+                # Plot the animation, where each frame shows the function and
+                # tangent line at which the gradient was evaluated
+                for i in range(len(x_vals)):
+                    grad = grad_vals[i]
+                    intercept = y_vals[i] - grad*x_vals[i]
+                    tangent_y = grad*t_vals + intercept
+                    plt.cla()
+                    plt.grid(True)
+                    plt.plot(t_vals, f_vals, label='Function')
+                    plt.plot(t_vals, tangent_y, '-r', label='Tangent')
+                    plt.xlim(t_vals[0], t_vals[-1])
+                    plt.ylim(min(f_vals), max(f_vals))
+                    plt.xlabel('x')
+                    plt.ylabel('f(x)')
+                    plt.legend()
+                    plt.pause(0.2)
+                    runtime -= 1
+
         
 
-    def stochastic_grad_descent(fn, x0, epsilon, max_iters, eta, train_size=1, batch_size=1):
-        '''
-        CURRENTLY NOT WORKING
-        '''
-        xn = x0
-        x = ad(1.0)
-        fn_parsed = x._parse_input(fn)
-        xn_history = []
-        for i in range(max_iters):
-            xn_history.append(xn)
-            # Calculate a gradient estimate
-            random_points = np.random.choice(train_size, batch_size)
-            grad_list = []
-            random_points = [0.1, 0.2, 0.3, 0.4, 0.5]
-            for point in random_points:
-                new_fxn = x._evaluate_function(fn_parsed, point)
-                grad_list.append(new_fxn.der[0])
-            batch_grad = np.mean(grad_list)
-            # Update the gradient
-            new_xn = xn - (eta / (i+1))*batch_grad
-            if abs(new_xn - xn) < epsilon:
-                print('Found solution after {} iterations.'.format(i))
-                print('Solution is: {}'.format(new_xn))
-                return new_xn
-            xn = new_xn
-            print(xn)
-        print('No solution found after max iterations.')
 
 
 
 
-f1 = 'x^5 - 5*x^2 + 2*x - 1'
+f1 = 'x^3 - 5*x^2 + 2*x - 1'
 f2 = 'x^2'
 f3 = 'x^3 - 3*x^2 + 4'
+f4 = 'tan(sin(x) + 3)'
+f5 = 'sin(x) + 5'
 #root, history = Optimize.newtons_method(f1, 2, 0.00001, max_iters=500)
-Optimize.animate_newtons(f3, 0.3, 0.000001, max_iters=500, runtime=20)
+#Optimize.animate_newtons(f3, 0.3, 0.000001, max_iters=500, runtime=20)
 #print(root, history)
 
-#Optimize.grad_descent(f1, 0, 0.00001, max_iters=500, eta=0.5)
+#Optimize.grad_descent(f4, 4, 0.00001, max_iters=500, eta=0.2)
+#Optimize.animate_grad_desc(f4, 4, 0.00001, max_iters=500, eta=0.3, runtime=20)
+#Optimize.animate_grad_desc(f4, 4, 0.00001, max_iters=500, eta=0.1, runtime=20)
+Optimize.animate_grad_desc(f4, 4, 0.00001, max_iters=500, eta=0.5, runtime=20)
+
 #Optimize.nesterov_grad_descent(f1, 0, 0.00001, max_iters=500, eta=0.5)
